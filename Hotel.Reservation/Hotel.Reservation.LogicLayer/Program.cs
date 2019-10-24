3a1f1867f824499c751efb92ea1b19a2cd4c222c
Diego Oliveira <diego.oliveira@dtidigital.com.br>
2019-10-24 15:45:24 -0300

    Removing files

---
+++ Hotel.Reservation/Hotel.Reservation.LogicLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hotel.Reservation.LogicLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
